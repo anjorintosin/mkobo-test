2c1c572c7ef0627dbc01a18d42c6d6eb3d08bd5e
Anjorin olutosin <anjorintosin080@gmail.com>
2023-09-15 16:45:17 +0100

    feat: final implementation

--- test/integration/sms.test.ts
+++ test/integration/sms.test.ts
@@ -1,3 +1,11 @@
+/**
+ * SMS Routes Test Suite
+ *
+ * This test suite covers the endpoints of the SMS Gateway API for processing inbound and outbound SMS messages.
+ * It uses the Supertest library to make HTTP requests to the server and Jest for assertions.
+ * Ensure that the server is running before executing these tests.
+ */
+
 const request = require('supertest');
 const expressTest = require('express');
 const { createServer } = require('http');
@@ -5,14 +13,14 @@ const { Pool } = require('pg');
 const smsRouter = require('../../src/routes/sms');
 const redisClient = require('../../src/config/redis');
 
-// Mock your database connection (pgClient)
+// Mock the PostgreSQL client to prevent actual database queries during testing
 const pgClientMock = new Pool();
 jest.mock('../../src/config/db', () => ({
   connect: () => pgClientMock,
-  query: () => ({ rowCount: 1 }), // Mock query result
+  query: () => ({ rowCount: 1 }), 
 }));
 
-// Mock Redis methods
+// Create an Express application for testing
 const app = expressTest();
 app.use(expressTest.json());
 app.use('/api', smsRouter);
@@ -33,6 +41,11 @@ describe('SMS Routes', () => {
     jest.clearAllMocks();
   });
 
+  /**
+   * Test Case: Should process inbound SMS with valid input
+   * It sends an HTTP POST request to the '/api/inbound/sms' endpoint with valid input data.
+   * Expects a 200 status code and a response matching { message: 'inbound sms ok', error: '' }.
+   */
   it('should process inbound SMS with valid input', async () => {
     const requestBody = {
       from: '91983435345',
@@ -51,6 +64,11 @@ describe('SMS Routes', () => {
     expect(response.body).toEqual({ message: 'inbound sms ok', error: '' });
   });
 
+  /**
+   * Test Case: Should process outbound SMS with valid input
+   * It sends an HTTP POST request to the '/api/outbound/sms' endpoint with valid input data.
+   * Expects a 200 status code and a response matching { message: 'outbound sms ok', error: '' }.
+   */
   it('should process outbound SMS with valid input', async () => {
     const requestBody = {
       from: '919823243432',
@@ -69,6 +87,11 @@ describe('SMS Routes', () => {
     expect(response.body).toEqual({ message: 'outbound sms ok', error: '' });
   });
 
+  /**
+   * Test Case: Should return error if 'to' datatype is not string
+   * It sends an HTTP POST request to the '/api/outbound/sms' endpoint with 'to' as a non-string value.
+   * Expects a 400 status code indicating a bad request.
+   */
   it('should return error if from datatype is not string', async () => {
     const requestBody = {
       from: '919823243432',
@@ -85,5 +108,4 @@ describe('SMS Routes', () => {
 
     expect(response.status).toBe(400);
   });
-
 });
--- test/unit/sms.test.ts
+++ test/unit/sms.test.ts
@@ -1,27 +1,35 @@
-import { isPhoneNumberAvaliable } from '../../src/utils/sms';
+import { checkFromParameter } from '../../src/utils/sms';
 import pgClient from "../../src/config/db";
-
 
 describe('Testing checkIfToExists function', () => {
 
   beforeEach(() => {
+    // Clear mock function calls before each test
     jest.clearAllMocks();
   });
 
-  it('should return true if check if found in the db', async () => {
-      jest.spyOn(pgClient, 'query').mockResolvedValueOnce({rowCount: 1} as never);
-      const result = await isPhoneNumberAvaliable('mockedUsername', 'mockedPhoneNumber', pgClient);
+  /**
+   * Test Case: Should return true if phone number is found in the database
+   * It spies on the 'query' method of the PostgreSQL client to mock a successful database query.
+   * Expects the utility function to return true.
+   */
+  it('should return true if phone number is found in the database', async () => {
+    jest.spyOn(pgClient, 'query').mockResolvedValueOnce({ rowCount: 1 } as never);
+    const result = await checkFromParameter('mockedUsername', 'mockedPhoneNumber', pgClient);
 
-      expect(result).toBe(true);
+    expect(result).toBe(true);
   });
- 
 
-  it('should return false if check if found', async () => {
-      jest.spyOn(pgClient, 'query').mockResolvedValueOnce({rowCount: 0} as never);
-      const result = await isPhoneNumberAvaliable('mockedUsername', 'mockedPhoneNumber', pgClient);
-  
-      expect(result).toBe(false);
+  /**
+   * Test Case: Should return false if phone number is not found in the database
+   * It spies on the 'query' method of the PostgreSQL client to mock an empty database query result.
+   * Expects the utility function to return false.
+   */
+  it('should return false if phone number is not found in the database', async () => {
+    jest.spyOn(pgClient, 'query').mockResolvedValueOnce({ rowCount: 0 } as never);
+    const result = await checkFromParameter('mockedUsername', 'mockedPhoneNumber', pgClient);
+
+    expect(result).toBe(false);
   });
 
 });
-
