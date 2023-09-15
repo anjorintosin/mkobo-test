9d0f4476f137874a984935c4e00ef3d314258c12
Anjorin olutosin <anjorintosin080@gmail.com>
2023-09-15 16:24:53 +0100

    feat: final implementation

--- src/utils/sms.ts
+++ src/utils/sms.ts
@@ -1,27 +1,3 @@
-import { Client } from 'pg';
-
-/**
- * Checks if a phone number is available for a specific user account.
- * @param {string} username - The username of the user account.
- * @param {string} to - The phone number to check availability for.
- * @param {Client} pgClient - The PostgreSQL database client.
- * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating whether the phone number is available.
- * @throws {Error} - Throws an error if there's an issue with the database query.
- */
-export async function isPhoneNumberAvaliable(username: string, to: string, pgClient: Client): Promise<boolean> {
-  try {    
-    const result = await pgClient.query('SELECT id FROM phone_number WHERE number = $1 AND account_id = (SELECT id FROM account WHERE username = $2)', [to, username]);    
-
-    if (result.rowCount >= 1) {      
-      return true;
-    } 
-    return false;
-  } catch (error) {
-    throw error;
-  }
-}
-
-
 /**
  * Checks if the 'from' parameter exists in the database.
  * @param {string} username - The username.
